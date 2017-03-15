e20b921d5b171aaae07e9ee0f0911fb49f49ec1e
MrSound <sound014@gmail.com>
2017-03-16 01:04:01 +0700

    edit signup

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -8,8 +8,7 @@ function tokenForUser(user) {
 }
 
 exports.signup = function (req, res, next) {
-    const email = req.body.email;
-    const password = req.body.password;
+    const { email, password } = req.body;
 
     if (!email || !password) {
         return res.status(422).send({ error: "You must provide email and password." });
